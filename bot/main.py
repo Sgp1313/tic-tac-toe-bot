99f18f2661d1667ca7e574255b210049168febbd
Angel Tórtola <tortola@ufm.edu>
2022-05-10 00:45:29 -0600

    [feat] Game flow refactored

--- bot/main.py
+++ bot/main.py
@@ -6,44 +6,59 @@ import utils
 def main():
 
     NAME = "Angel"
-    print("------- Starting tic-tac-toe bot -------")
-    sleep(1)
+    print("\n------- Starting tic-tac-toe bot -------\n")
 
-    # Register-phase begins
-    registry_available = utils.is_registry_available()
+    # Register phase begins
+    registry_open = utils.is_registry_open()
 
-    while not registry_available:
+    while not registry_open:
+        print("Waiting for registry to open...\n")
         sleep(5)
+        registry_open = utils.is_registry_open()
 
-    player_id = utils.register_user(name)
-    print("Player ID:", player_id)
-    # Register-phase ends, game-phase begins
-    game_continues = True
+    # Register is open now, let's register as player
+    PLAYER_ID = utils.register_user(NAME)
+    print("Registered successfully as {}, player ID is: {}\n".format(NAME, PLAYER_ID))
+    sleep(2)
+
+    # Game-continues flag, set to True until there's a winner
+    game_continues = utils.does_game_continue()
 
     while game_continues:
 
-        my_turn = utils.is_my_turn(player_id)
+        my_turn = utils.is_my_turn(PLAYER_ID)
         
+        # Flag to waiti for our turn
         while not my_turn:
-            print("My turn:", my_turn)
+            print("Waiting for turn...")
             sleep(5)
-            my_turn = utils.is_my_turn(player_id)
+            my_turn = utils.is_my_turn(PLAYER_ID)
 
-        print("My turn:", my_turn)
+        # It's our turn, start by reading the latest version of the board
         board = utils.read_board()
-        print("Board:", board)
-        is_move_valid = False
+        utils.print_board(board)
 
-        while not is_move_valid:
+        # Entering cycle of deciding-validating our next move
+        valid_move = False
 
-            next_move = utils.decide_move(board, player_id)
-            print("Move:", next_move)
-            is_move_valid = utils.validate_move(board, next_move)
+        while not valid_move:
 
-        utils.send_move(next_move)
-        game_continues = utils.does_game_continue()
+            print("Deciding move...\n")
+            sleep(1)
+            next_move = utils.decide_move(board, PLAYER_ID)
+            valid_move = utils.validate_move(board, next_move) # Validates next move
+
+        print("Move to send, row: {}, col: {}\n".format(next_move[0], next_move[1]))
+
+        # Send move to API and check if game continues
+        utils.send_move(PLAYER_ID, next_move)
+
+        # Wait and update game_continues flag
         sleep(5)
+        game_continues = utils.does_game_continue()
 
+    # Game ends, check API for winner
+    print("GAME OVER\nCheck API for winner.")
 
 if __name__ == '__main__':
     main()

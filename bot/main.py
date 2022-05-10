79a8c5ef9d9f6dc827704316e69c2000879b94c1
Angel Tórtola <tortola@ufm.edu>
2022-05-09 23:01:43 -0600

    [feat] Main game flow implemented

--- bot/main.py
+++ bot/main.py
@@ -2,9 +2,52 @@
 Main script.
 '''
 
+from time import sleep 
+import requests
 import utils
 
 
-if __name__ == 'main':
-    print('hi')
-    
+def main():
+
+    name = 'Angel'
+    print('------- Starting tic-tac-toe bot -------')
+    sleep(1)
+
+    # Register-phase begins
+    registry_available = utils.is_registry_available()
+
+    while not registry_available:
+        sleep(5)
+
+    player_id = utils.register_user(name)
+    print("Player ID:", player_id)
+    # Register-phase ends, game-phase begins
+    game_continues = True
+
+    while game_continues:
+
+        my_turn = utils.is_my_turn(player_id)
+        
+        while not my_turn:
+            print("My turn:", my_turn)
+            sleep(5)
+            my_turn = utils.is_my_turn(player_id)
+
+        print("My turn:", my_turn)
+        board = utils.read_board()
+        print("Board:", board)
+        is_move_valid = False
+
+        while not is_move_valid:
+
+            next_move = utils.decide_move(board, player_id)
+            print("Move:", next_move)
+            is_move_valid = utils.validate_move(board, next_move)
+
+        utils.send_move(next_move)
+        game_continues = utils.does_game_continue()
+        sleep(5)
+
+
+if __name__ == '__main__':
+    main()
--- bot/utils.py
+++ bot/utils.py
@@ -1,32 +1,47 @@
 '''
-Utils module.
+[Module] Tic-tac-toe bot utilities.
 '''
+
 from random import randint
+import requests
 
-def is_registry_available() -> bool:
+
+def is_registry_open() -> bool:
     '''
-    Returns: True | False
+    Checks if registry is available via API.
     '''
+    res = requests.get('http://127.0.0.1:8000/registry')
 
-    is_available = True
-    return is_available
+    if res.text == 'true':
+        is_open = True
+    elif res.text == 'false':
+        is_open = False
+
+    return is_open
 
 
-def register_user(name: str) -> int:
+def register_user(name: str) -> str:
     '''
-    Returns: str [player ID]
+    Registers user in API game.
     '''
+    res = requests.post('http://127.0.0.1:8000/register_player/{}'.format(name))
+    player_id = res.text
 
-    player = "X"
-    return player
+    return player_id
 
 
-def is_my_turn(player: str) -> bool: 
+def is_my_turn(player_id: str) -> bool: 
     '''
-    Returns: True | False
+    Checks player's turn via API.
     '''
-    my_turn = True
-    pass my_turn
+    res = requests.get('http://127.0.0.1:8000/turn/{}'.format(player_id))
+    
+    if res.text == 'true':
+        my_turn = True
+    elif res.text == 'false':
+        my_turn = False
+
+    return my_turn
 
 
 def read_board() -> list:
@@ -41,8 +56,8 @@ def decide_move(board: list, player: str) -> [int, int]:
     '''
     Returns: a list of two ints
     '''
-    x = randint(0, 3)
-    y = randint(0, 3)
+    x = randint(0, 2)
+    y = randint(0, 2)
 
     return [x, y]
 
@@ -51,7 +66,8 @@ def validate_move(board: list, move: list) -> bool:
     '''
     Returns: True | False
     '''
-    x, y = board[move[0]], board[move[1]]
+    # x = fila, y = columna
+    x, y = move[0], move[1]
 
     if board[x][y] is None:
         return True
@@ -59,10 +75,10 @@ def validate_move(board: list, move: list) -> bool:
     return False
 
 
-def send_move():
+def send_move(move: list) -> None:
     
     # Sending move
-    return True
+    return None
 
 
 def does_game_continue() -> bool:
@@ -71,3 +87,8 @@ def does_game_continue() -> bool:
     '''
     game_continues = True
     return game_continues
+
+
+# print(is_registry_open(), type(is_registry_open()))
+# print(register_user("Pedro"))
+print(is_my_turn("O"))
